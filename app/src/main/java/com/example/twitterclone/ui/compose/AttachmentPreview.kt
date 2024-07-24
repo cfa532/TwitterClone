16f7cc03c6cf7dab6ae1ca6f77f80b6c2ba1c281
cfa532 <cfa532@yahoo.com>
2024-07-24 11:07:09 +0800

    thumbnail worked

--- app/src/main/java/com/example/twitterclone/ui/compose/AttachmentPreview.kt
+++ app/src/main/java/com/example/twitterclone/ui/compose/AttachmentPreview.kt
@@ -3,29 +3,37 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Rect
 import android.net.Uri
-import android.provider.MediaStore
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Icon
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
+@RequiresApi(Build.VERSION_CODES.Q)
 @Composable
 fun AttachmentIcon(uri: Uri) {
-    val configuration = LocalConfiguration.current
-    val screenWidth = configuration.screenWidthDp
-    val canvasSize = screenWidth / 2
-    val contentResolver = LocalContext.current.contentResolver
+    val view = LocalView.current
+    val viewWidth = with(LocalDensity.current) { view.width.toDp() }.value.toInt()
+    val canvasSize = viewWidth / 2
     var imageBitmap by remember { mutableStateOf<ImageBitmap?>(null) }
+    val contentResolver = LocalContext.current.contentResolver
 
     LaunchedEffect(uri) {
         withContext(Dispatchers.IO) {
@@ -33,7 +41,8 @@ fun AttachmentIcon(uri: Uri) {
                 val bitmap = contentResolver.loadThumbnail(
                     uri, android.util.Size(canvasSize, canvasSize), null
                 )
-                val resizedBitmap = Bitmap.createBitmap(canvasSize, canvasSize, Bitmap.Config.ARGB_8888)
+                val resizedBitmap =
+                    Bitmap.createBitmap(canvasSize, canvasSize, Bitmap.Config.ARGB_8888)
                 val canvas = Canvas(resizedBitmap)
                 val paint = Paint(Paint.ANTI_ALIAS_FLAG)
                 val aspectRatio = bitmap.width.toFloat() / bitmap.height.toFloat()
@@ -49,10 +58,10 @@ fun AttachmentIcon(uri: Uri) {
                 val xOffset = (canvasSize - scaledWidth) / 2
                 val yOffset = (canvasSize - scaledHeight) / 2
                 canvas.drawBitmap(
-                        bitmap,
-                null,
-                Rect(xOffset, yOffset, xOffset + scaledWidth, yOffset + scaledHeight),
-                paint
+                    bitmap,
+                    null,
+                    Rect(xOffset, yOffset, xOffset + scaledWidth, yOffset + scaledHeight),
+                    paint
                 )
 
                 imageBitmap = resizedBitmap.asImageBitmap()
