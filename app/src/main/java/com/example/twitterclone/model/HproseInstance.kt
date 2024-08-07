c85a4a78d4f52d70991663778b17237438ea68eb
cfa532 <cfa532@yahoo.com>
2024-08-07 09:45:48 +0800

    fixed build issue

--- app/src/main/java/com/example/twitterclone/model/HproseInstance.kt
+++ app/src/main/java/com/example/twitterclone/model/HproseInstance.kt
@@ -21,43 +21,6 @@ import java.io.InputStream
 import java.math.BigInteger
 import java.net.URL
 
-interface HproseService {
-    fun getVarByContext(sid: String, context: String, mapOpt: Map<String, String>? = null): String
-    fun login(ppt: String): Map<String, String>
-    fun getVar(sid: String, name: String, arg1: String? = null, arg2: String? = null): String
-    fun mmCreate(
-        sid: String,
-        appId: String,
-        ext: String,
-        mark: String,
-        tp: Byte,
-        right: Long
-    ): MimeiId
-
-    fun mmOpen(sid: String, mid: MimeiId, version: String): String
-    fun mmBackup(
-        sid: String,
-        mid: MimeiId,
-        memo: String = "",
-        ref: String = ""
-    ) // Add default value for 'ref'
-
-    fun mmAddRef(sid: String, mid: MimeiId, mimeiId: MimeiId)
-    fun mmSetObject(fsid: String, obj: Any)
-    fun mimeiPublish(sid: String, memo: String, mid: MimeiId)
-    fun mfOpenTempFile(sid: String): String
-    fun mfTemp2Ipfs(fsid: String, ref: MimeiId): MimeiId
-    fun mfSetCid(sid: String, mid: MimeiId, cid: MimeiId)
-    fun mfSetData(fsid: String, data: ByteArray, offset: Int)
-    fun set(sid: String, key: String, value: Any)
-    fun get(sid: String, key: String): Any?
-    fun hGet(sid: String, key: String, field: String): Any?
-    fun hSet(sid: String, key: String, field: String, value: Any)
-    fun hDel(sid: String, key: String, field: String)
-    fun zAdd(sid: String, key: String, sp: ScorePair)
-    fun zRevRange(sid: String, key: String, start: Long, end: Long): List<*>
-}
-
 // Encapsulate Hprose client and related operations in a singleton object.
 object HproseInstance {
     private const val BASE_URL = "http://192.168.0.61:8081"
@@ -325,4 +288,41 @@ object HproseInstance {
         }
         return R.drawable.ic_user_avatar
     }
+}
+
+interface HproseService {
+    fun getVarByContext(sid: String, context: String, mapOpt: Map<String, String>? = null): String
+    fun login(ppt: String): Map<String, String>
+    fun getVar(sid: String, name: String, arg1: String? = null, arg2: String? = null): String
+    fun mmCreate(
+        sid: String,
+        appId: String,
+        ext: String,
+        mark: String,
+        tp: Byte,
+        right: Long
+    ): MimeiId
+
+    fun mmOpen(sid: String, mid: MimeiId, version: String): String
+    fun mmBackup(
+        sid: String,
+        mid: MimeiId,
+        memo: String = "",
+        ref: String = ""
+    ) // Add default value for 'ref'
+
+    fun mmAddRef(sid: String, mid: MimeiId, mimeiId: MimeiId)
+    fun mmSetObject(fsid: String, obj: Any)
+    fun mimeiPublish(sid: String, memo: String, mid: MimeiId)
+    fun mfOpenTempFile(sid: String): String
+    fun mfTemp2Ipfs(fsid: String, ref: MimeiId): MimeiId
+    fun mfSetCid(sid: String, mid: MimeiId, cid: MimeiId)
+    fun mfSetData(fsid: String, data: ByteArray, offset: Int)
+    fun set(sid: String, key: String, value: Any)
+    fun get(sid: String, key: String): Any?
+    fun hGet(sid: String, key: String, field: String): Any?
+    fun hSet(sid: String, key: String, field: String, value: Any)
+    fun hDel(sid: String, key: String, field: String)
+    fun zAdd(sid: String, key: String, sp: ScorePair)
+    fun zRevRange(sid: String, key: String, start: Long, end: Long): List<*>
 }
