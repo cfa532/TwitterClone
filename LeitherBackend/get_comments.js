9c4d738a18c6600cd703a38a45dea461b889fa51
cfa532 <cfa532@yahoo.com>
2024-08-26 00:18:44 +0800

    get comments JS bug

---
+++ LeitherBackend/get_comments.js
@@ -0,0 +1,22 @@
+((request, args)=>{
+try {
+    const COMMENT_LIST = "comment_list_key"
+    const TWT_CONTENT_KEY = "core_data_of_tweet"
+
+    let tweetId = request["tweetid"]
+    let mmsid = lapi.MMOpen("", tweetId, "last")
+    console.log("tweetId=", tweetId, mmsid)
+
+    let arr = lapi.Zrevrange(mmsid, COMMENT_LIST, 0, -1)
+    console.log(arr.length)
+    console.log("arr[0]=",arr[0])
+    return arr.map(sp => {
+        console.log("sp=", JSON.stringify(sp))
+        let csid = lapi.MMOpen("", sp.member, "last")
+        let tweet = lapi.Get(csid, TWT_CONTENT_KEY)
+        return tweet
+    })
+} catch(e) {
+    return e
+}
+})(request, args)
