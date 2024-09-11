66fd83188aa4f1018680a9b6735efad4f2e28621
cfa532 <cfa532@yahoo.com>
2024-09-11 15:16:54 +0800

    Update const.js

--- LeitherBackend/const.js
+++ LeitherBackend/const.js
@@ -12,10 +12,7 @@ export const ORIGINAL_TWEET = "original_tweet_mid"
 export const ATTACHMENTS = "attachements_key"
 export const PRIVACY = "privacy_key"
 export const TWT_CONTENT_KEY = "core_data_of_tweet"
-
-export const APP_ID = "V6MUd0cVeuCFE7YsGLNn5ygyJlm"
-export const APP_EXT = "com.example.twitterclone"
-export const APP_MARK = "version 0.0.4"
+const CHAT_SESSION_LIST = "chat_session_list"
 
 // Keys in App mimei database
 export const TWT_LIST_KEY = "list_of_tweets_mid"
