a2a56964e3190701735b16a21ab8e5f84ce5b616
cfa532 <cfa532@yahoo.com>
2024-09-15 06:10:16 +0800

    Update init_user_mid.js

--- LeitherBackend/init_user_mid.js
+++ LeitherBackend/init_user_mid.js
@@ -3,7 +3,6 @@
     const APP_EXT = "com.example.twitterclone"
     let APP_MARK = "στηναρχή"
 
-    const FOLLOWINGS_KEY = "list_of_followings_mid"
     const OWNER_DATA_KEY = "data_of_author"
     const BOOKMARK_COUNT = "tweet_bookmark_count"
     const LIKE_COUNT = "tweet_like_count"
